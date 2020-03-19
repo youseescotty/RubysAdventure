8f7ce1780ba65d89d8cd7c318e9b434ed7e88923
Scott Livingston <scottlivingston@Scottys-Macbook-Air.local>
2020-03-19 17:31:05 -0500

    HUD
    Added Health Bar

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -79,7 +79,7 @@ public class RubyController : MonoBehaviour
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 
     void Launch()
